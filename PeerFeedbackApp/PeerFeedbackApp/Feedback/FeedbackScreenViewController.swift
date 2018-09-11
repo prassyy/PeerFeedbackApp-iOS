594df1f677f7e84e7f4a9195889379052f6b16a6
Prassyy <skprasannakumar92@gmail.com>
2018-09-11 13:13:49 +0530

    Adds Picker view to choose developer

--- PeerFeedbackApp/PeerFeedbackApp/Feedback/FeedbackScreenViewController.swift
+++ PeerFeedbackApp/PeerFeedbackApp/Feedback/FeedbackScreenViewController.swift
@@ -10,25 +10,57 @@ import UIKit
 
 class FeedbackScreenViewController: UIViewController {
     @IBOutlet weak var feedbackTableView: UITableView!
+    @IBOutlet weak var peerPicker: UIPickerView!
     
     override func viewDidLoad() {
         feedbackTableView.delegate = self
         feedbackTableView.dataSource = self
+        feedbackTableView.tableHeaderView = headerView()
+
+        peerPicker.isHidden = true
     }
 }
 
 extension FeedbackScreenViewController: UITableViewDelegate, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = feedbackTableView.dequeueReusableCell(withIdentifier: "FeedbackTableViewCell", for: indexPath)
-        cell.textLabel?.text = "EV FB4 Peer Feedback"
-        return cell
+        switch indexPath.section {
+        case 0:
+            return chooseDeveloperCell(for: indexPath)
+        default:
+            return UITableViewCell()
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
+    }
+
+    private func headerView() -> UIView {
+        let nib = UINib(nibName: "TitleView", bundle: Bundle(for: type(of: self)))
+        let viewsList = nib.instantiate(withOwner: self, options: nil)
+        if viewsList.count > 0,
+            let headerView = viewsList[0] as? UIView {
+            return headerView
+        }
+        return UIView()
+    }
+    
+    private func chooseDeveloperCell(for index: IndexPath) -> UITableViewCell {
+        let cell = feedbackTableView.dequeueReusableCell(withIdentifier: "ChooseDeveloperTableViewCell", for: index)
+        cell.textLabel?.text = "Developer"
+        cell.detailTextLabel?.text = "Choose"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            tableView.deselectRow(at: indexPath, animated: true)
+            peerPicker.isHidden = false
+        }
     }
 }
--- PeerFeedbackApp/PeerFeedbackAppTests/Feedback/FeedbackScreenViewControllerSpec.swift
+++ PeerFeedbackApp/PeerFeedbackAppTests/Feedback/FeedbackScreenViewControllerSpec.swift
@@ -23,24 +23,41 @@ class FeedbackScreenViewControllerSpec: XCTestCase {
         XCTAssertNotNil(tableView)
     }
     
+    func test_containsPickerViewAndMustBeHidden() {
+        let pickerView = subject.view.getViewsOfType(UIPickerView.self).first
+        XCTAssertNotNil(pickerView)
+        XCTAssert(pickerView!.isHidden)
+    }
+    
     func test_tableViewContainsHeaderWithAppName() {
         let tableView = subject.view.getViewsOfType(UITableView.self).first
         tableView?.reloadData()
         
-        let headerCell = tableView!.cellForRow(at: IndexPath(item: 0, section: 0))
-        XCTAssertNotNil(headerCell)
+        let headerView = tableView!.tableHeaderView
+        XCTAssertNotNil(headerView)
         
-        XCTAssertEqual(headerCell?.textLabel?.text, "EV FB4 Peer Feedback")
+        let titleLabel = headerView?.getViewsOfType(UILabel.self).first(where: { $0.text == "EV FB4 Peer Feedback"})
+        XCTAssertNotNil(titleLabel)
     }
     
     func test_tableViewContainsCellToChooseDeveloper() {
         let tableView = subject.view.getViewsOfType(UITableView.self).first
         tableView?.reloadData()
         
-        let developerChoiceCell = tableView!.cellForRow(at: IndexPath(item: 0, section: 1))
+        let developerChoiceCell = tableView!.cellForRow(at: IndexPath(item: 0, section: 0))
         XCTAssertNotNil(developerChoiceCell)
         XCTAssertEqual(developerChoiceCell?.textLabel?.text, "Developer")
         XCTAssertEqual(developerChoiceCell?.detailTextLabel?.text, "Choose")
+    }
+    
+    func test_chooseDeveloperCellTapShowsPicker() {
+        let tableView = subject.view.getViewsOfType(UITableView.self).first!
+        let pickerView = subject.view.getViewsOfType(UIPickerView.self).first
+
+        tableView.reloadData()
+        tableView.delegate?.tableView!(tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+
+        XCTAssertFalse(pickerView!.isHidden)
     }
 }
 
